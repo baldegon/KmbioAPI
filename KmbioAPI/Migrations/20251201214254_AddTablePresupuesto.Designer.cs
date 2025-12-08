a2788a50ac79806589b68ee8b1495cb2415cc0a5
Bladimir Holmann Zink <bladimirzink7@gmail.com>
2025-12-08 16:23:11 -0300

    agregar tabla presupuesto

--- KmbioAPI/Migrations/20251201214254_AddTablePresupuesto.Designer.cs
+++ KmbioAPI/Migrations/20251201214254_AddTablePresupuesto.Designer.cs
@@ -4,6 +4,7 @@ using KmbioAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KmbioAPI.Migrations
 {
     [DbContext(typeof(KmbioDbContext))]
-    partial class KmbioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251201214254_AddTablePresupuesto")]
+    partial class AddTablePresupuesto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
